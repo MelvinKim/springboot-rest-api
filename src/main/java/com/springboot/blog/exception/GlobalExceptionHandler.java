e4113b3be9e663ce3ae839eab066a57087eef5c6
MelvinKim <melobruk1@gmail.com>
2022-05-30 23:35:29 +0300

    Exception handling: custom exception handling and global exception handling

---
+++ src/main/java/com/springboot/blog/exception/GlobalExceptionHandler.java
@@ -0,0 +1,47 @@
+package com.springboot.blog.exception;
+
+import com.springboot.blog.payload.ErrorDetails;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Date;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    //handle specific exceptions
+    //1.handle ResourceNotFoundException
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorDetails> handleResourceNotFoundException(ResourceNotFoundException exception,
+                                                                        WebRequest webRequest) {
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(),
+                webRequest.getDescription(false));
+
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+    //2.handle BlogApiException
+    @ExceptionHandler(BlogAPIException.class)
+    public  ResponseEntity<ErrorDetails> handleBlogAPIException(BlogAPIException exception,
+                                                                WebRequest webRequest) {
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(),
+                webRequest.getDescription(false));
+
+        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+    }
+
+
+    //handle global exceptions
+    //eg when passing postId or commentId as a string instead of an integer
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDetails> handleGlobalException(Exception exception,
+                                                               WebRequest webRequest) {
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(),
+                webRequest.getDescription(false));
+
+        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
